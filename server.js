a47f147ea9dde6effd5832c7470421bbd7f9fe77
ConOLeary <conolaoghaire@gmail.com>
2020-04-05 16:41:28 +0100

    sample local node server runnable

---
+++ server.js
@@ -0,0 +1,13 @@
+var http = require('http');
+var dt = require('myfirstmodule');
+
+http.createServer(function (req, res) {
+  res.writeHead(200, {'Content-Type': 'text/html'});
+  res.write("The date and time is currently: " + dt.myDateTime());
+  res.end();
+}).listen(8080);
+
+/*// Storing data:
+myObj = { name: "John", age: 31, city: "New York" };
+myJSON = JSON.stringify(myObj);
+localStorage.setItem("testJSON", myJSON);*/
