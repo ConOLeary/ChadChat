088e28caabaddeaf27e88996505471f4bb525397
ConOLeary <conolaoghaire@gmail.com>
2020-04-11 13:58:05 +0100

    new dependancies; playing around with Twitter API; some CRLFs being replaced by LF

--- server.js
+++ server.js
@@ -1,10 +1,28 @@
-var http = require('http');
-var dt = require('myfirstmodule');
+const http = require('http');
+const https = require('https');
 
 http.createServer(function (req, res) {
   res.writeHead(200, {'Content-Type': 'text/html'});
-  res.write("The date and time is currently: " + dt.myDateTime());
+  res.write("The date and time is currently: " + Date());
+
+  https.get('https://api.twitter.com/1.1/search/tweets.json?q=%23yolo', (resp) => {
+    let data = '';
+
+    // A chunk of data has been recieved.
+    resp.on('data', (chunk) => {
+      data += chunk;
+    });
+
+    // The whole response has been received. Print out the result.
+    resp.on('end', () => {
+      console.log(JSON.parse(data).explanation);
+    });
+
+  }).on("error", (err) => {
+    console.log("Error: " + err.message);
+  });
   res.end();
+
 }).listen(8080);
 
 /*// Storing data:
