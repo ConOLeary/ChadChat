09d102c61d74b1c26a3ae236bdd757e8af0e4b09
ConOLeary <conolaoghaire@gmail.com>
2020-04-11 17:54:14 +0100

    first Twitter data pull working

--- server.js
+++ server.js
@@ -1,26 +1,24 @@
-const http = require('http');
-const https = require('https');
+const http= require('http');
+const https= require('https');
+var Twit= require('twit');
+var config= require('./config');
+var access= new Twit(config);
+
+var params= {
+  q: 'rainbow',
+  count: 2
+}
+
+access.get('search/tweets', params, gotData);
+function gotData(err, data, response){
+  console.log(data)
+}
+
+
 
 http.createServer(function (req, res) {
   res.writeHead(200, {'Content-Type': 'text/html'});
   res.write("The date and time is currently: " + Date());
-
-  https.get('https://api.twitter.com/1.1/search/tweets.json?q=%23yolo', (resp) => {
-    let data = '';
-
-    // A chunk of data has been recieved.
-    resp.on('data', (chunk) => {
-      data += chunk;
-    });
-
-    // The whole response has been received. Print out the result.
-    resp.on('end', () => {
-      console.log(JSON.parse(data).explanation);
-    });
-
-  }).on("error", (err) => {
-    console.log("Error: " + err.message);
-  });
   res.end();
 
 }).listen(8080);
